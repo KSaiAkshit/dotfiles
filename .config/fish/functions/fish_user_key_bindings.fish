f839220a52d1335186d723d936ed6a50d200e75a
Sai Akshit <44054643+KSaiAkshit@users.noreply.github.com>
2022-12-11 09:25:31 +0530

    normal update

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
   fzf_key_bindings
   bind -M insert -m default jj backward-char force-repaint
-  bind \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+  # bind \co 'commandline ranger-cd; commandline -f execute'
+  bind \co 'set old_tty (stty -g); stty sane; ranger-cd; stty $old_tty; commandline -f repaint'
 end
