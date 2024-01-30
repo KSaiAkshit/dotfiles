9bd2fa147c8456ab2ea42a5281e1bac02c1f61f4
Akira <ksaiashit@gmail.com>
2024-01-30 22:26:28 +0530

    update

--- nvim/.config/nvim/lua/plugins/arduino.lua
+++ nvim/.config/nvim/lua/plugins/arduino.lua
@@ -1,22 +1,34 @@
 local M = {
-	"glebzlat/Arduino.nvim",
-	ft = "arduino",
-	config = function()
-		require('arduino').setup {
-			default_fqbn = "arduino:avr:uno",
-
-			--Path to clangd (all paths must be full)
-			clangd = require 'mason-core.path'.bin_prefix 'clangd',
-			--Path to arduino-cli
-			arduino = "/usr/bin/arduino-cli",
-
-			--Data directory of arduino-cli
-			arduino_config_dir = "/home/akira/.local/share/arduino15/",
-		}
-
-		require 'lspconfig' ['arduino_language_server'].setup {
-			on_new_config = arduino.on_new_config,
-		}
-	end
+  "stevearc/vim-arduino",
+  ft = "arduino",
+  keys = {
+    { "<leader>aa", "<cmd>ArduinoAttach<CR>", desc = "Automatically attach to your board" },
+    { "<leader>av", "<cmd>ArduinoVerify<CR>", desc = "Build the sketch" },
+    { "<leader>au", "<cmd>ArduinoUpload<CR>", desc = "Build and upload the sketch" },
+    { "<leader>aus", "<cmd>ArduinoUploadAndSerial<CR>", desc = "Build, upload, and connect for debugging" },
+    { "<leader>as", "<cmd>ArduinoSerial<CR>", desc = "Connect to the board for debugging over a serial port" },
+    {
+      "<leader>ab",
+      "<cmd>ArduinoChooseBoard<CR>",
+      desc = "Select the type of board. With no arg, will present a choice dialog",
+    },
+    {
+      "<leader>ap",
+      "<cmd>ArduinoChooseProgrammer<CR>",
+      desc = "Select the programmer. With no arg, will present a choice dialog",
+    },
+  },
+  config = function()
+    require("lspconfig").arduino_language_server.setup({
+      filetypes = { "arduino" },
+      cmd = {
+        "arduino-language-server",
+        "-cli",
+        "/usr/bin/arduino-cli",
+        "-cli-config",
+        "/home/akira/.arduino15/arduino-cli.toml",
+      },
+    })
+  end,
 }
 return M
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,15 +5,15 @@ local config = {}
 config.colors = colors
 config.enable_tab_bar = false
 config.font_size = 10.0
-config.enable_wayland = false
+-- config.enable_wayland = false
 
 
 -- config.font = wezterm.font("JetBrainsMono NF")
-config.font = wezterm.font("Monaspace Neon")
+config.font = wezterm.font("MonaspiceNe Nerd Font")
 -- config.font_size = 11.0
 
 config.window_background_opacity = 0.6
-config.default_prog = { "/usr/bin/zellij", "a", "-c" }
+-- config.default_prog = { "/usr/bin/zellij", "a", "-c" }
 config.window_close_confirmation = 'NeverPrompt'
 config.warn_about_missing_glyphs = false
 
