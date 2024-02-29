fb507103aaee8f23e6778adf1d84f99cddcd67fd
Akira <ksaiashit@gmail.com>
2024-02-29 23:17:27 +0530

    update: update scripts, add keymaps
    update

--- dot_config/LazyVim/lua/config/keymaps.lua
+++ dot_config/LazyVim/lua/config/keymaps.lua
@@ -4,12 +4,23 @@
 local map = vim.keymap.set
 local del = vim.keymap.del
 
+map("x", "<leader>p", [["_dP]])
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+map({ "n", "v" }, "<leader>d", [["_d]])
+
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 map("i", "<C-h>", "<Left>")
 map("i", "<C-l>", "<Right>")
 map("i", "<C-j>", "<Down>")
 map("i", "<C-k>", "<Up>")
+
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 del("n", "<A-j>")
 del("n", "<A-k>")
