9f28b20bede1ce0e5001de82ac7332d6c564eef6
Akira <ksaiashit@gmail.com>
2024-05-15 01:31:49 +0530

    update: hypr

--- dot_config/hypr/scripts/executable_PywalSwww.sh
+++ dot_config/hypr/scripts/executable_PywalSwww.sh
@@ -9,30 +9,27 @@ monitor_outputs=($(ls "$cache_dir"))
 # Initialize a flag to determine if the ln command was executed
 ln_success=false
 
-# Loop through monitor outputs
-for output in "${monitor_outputs[@]}"; do
-    # Construct the full path to the cache file
-    cache_file="$cache_dir$output"
-
-    # Check if the cache file exists for the current monitor output
-    if [ -f "$cache_file" ]; then
-        # Get the wallpaper path from the cache file
-        wallpaper_path=$(cat "$cache_file")
-
-        # Copy the wallpaper to the location Rofi can access
-        if ln -sf "$wallpaper_path" "$HOME/.config/rofi/.current_wallpaper"; then
-            ln_success=true  # Set the flag to true upon successful execution
-        fi
-
-        break  # Exit the loop after processing the first found monitor output
+# Get current focused monitor
+current_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
+echo $current_monitor
+# Construct the full path to the cache file
+cache_file="$cache_dir$current_monitor"
+echo $cache_file
+# Check if the cache file exists for the current monitor output
+if [ -f "$cache_file" ]; then
+    # Get the wallpaper path from the cache file
+    wallpaper_path=$(cat "$cache_file")
+    echo $wallpaper_path
+    # Copy the wallpaper to the location Rofi can access
+    if ln -sf "$wallpaper_path" "$HOME/.config/rofi/.current_wallpaper"; then
+        ln_success=true  # Set the flag to true upon successful execution
     fi
-done
+fi
 
 # Check the flag before executing further commands
 if [ "$ln_success" = true ]; then
-    # execute pywal
-    # wal -i "$wallpaper_path"
-
-    # execute pywal skipping tty and terminal changes
+    # execute wallust
+	echo 'about to execute pywal'
+    # execute wallust skipping tty and terminal changes
     wal -i "$wallpaper_path" -s -t &
 fi
