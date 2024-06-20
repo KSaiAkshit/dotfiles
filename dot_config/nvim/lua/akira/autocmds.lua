0d7fc60fcba7166bc69a0e66f4f402f08237d824
Akira <ksaiashit@gmail.com>
2024-06-21 00:27:27 +0530

    nvim: move to mini.icons

--- dot_config/nvim/lua/akira/autocmds.lua
+++ dot_config/nvim/lua/akira/autocmds.lua
@@ -1,11 +1,3 @@
--- Transparent background
--- vim.api.nvim_create_autocmd({ "" }, {
--- 	pattern = '*',
--- 	callback = function()
--- 		vim.api.nvim_command('highlight Normal guibg=none ctermbg=none')
--- 		vim.api.nvim_command('highlight NormalFloat guibg=none ctermbg=none')
--- 	end
--- })
 -- MiniBasics Autocommands
 local augroup = vim.api.nvim_create_augroup('MiniBasicsAutoCommands', {})
 
@@ -94,10 +86,28 @@ vim.api.nvim_create_autocmd('User', {
 vim.api.nvim_create_autocmd({ "LspAttach" }, {
   callback = function()
     local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = signs.Error,
+          [vim.diagnostic.severity.WARN] = signs.Warn,
+          [vim.diagnostic.severity.HINT] = signs.Hint,
+          [vim.diagnostic.severity.INFO] = signs.INFO,
+        },
+        texthl = {
+          [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+          [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+          [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+          [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+        },
+        numhl = {
+          [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+          [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+          [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+          [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+        }
+      }
+    })
   end
 })
 -- FTP autocommands
